7e920eac5348bbc90b2a37513925195210ccbb19
sangminlee98 <tkdals0975@naver.com>
2022-05-01 22:31:44 +0900

    basic animations

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
+import { motion } from 'framer-motion';
 
 const Wrapper = styled.div`
   height: 100vh;
@@ -8,18 +9,19 @@ const Wrapper = styled.div`
   justify-content: center;
   align-items: center;
 `;
-const Box = styled.div`
+const Box = styled(motion.div)`
   width: 200px;
   height: 200px;
   background-color: white;
-  border-radius: 10px;
+  border-radius: 15px;
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
 `;
 
 function App() {
   return (
     <Wrapper>
-      <Box />
+      {/* <Box transition={{duration: .5}} initial={{transform:'translateY(50px)', opacity:0}} animate={{transform: 'translateY(0px)', opacity: 1}}/> */}
+      <Box initial={{scale: 0}} animate={{scale: 1, rotateZ: 360}}/>
     </Wrapper>
   );
 }
