1191c1947da0f1e965c0e7a180e80e125798bbdb
sangminlee98 <tkdals0975@naver.com>
2022-05-09 19:34:59 +0900

    SVG animation

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 import { motion, useMotionValue, useTransform, useViewportScroll } from 'framer-motion';
 
 const Wrapper = styled(motion.div)`
-  height: 200vh;
+  height: 100vh;
   width: 100vw;
   display: flex;
   justify-content: center;
@@ -19,13 +19,13 @@ const Wrapper = styled(motion.div)`
 //   align-items: center;
 //   overflow: hidden;
 // `;
-const Box = styled(motion.div)`
-  width: 200px;
-  height: 200px;
-  background-color: rgba(255,255,255,1);
-  border-radius: 30px;
-  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
-`;
+// const Box = styled(motion.div)`
+//   width: 200px;
+//   height: 200px;
+//   background-color: rgba(255,255,255,1);
+//   border-radius: 30px;
+//   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
+// `;
 // const Circle = styled(motion.div)`
 //   background-color: white;
 //   height: 70px;
@@ -67,27 +67,56 @@ const Box = styled(motion.div)`
 //   },
 // }
 
-const boxVariant = {
-  // hover: {scale: 1.5, rotateZ: 90},
-  // tap: {borderRadius: '100px', scale: 1},
-  // drag: {
-  //   backgroundColor: 'rgb(46,204,113)',
-  //   transition: {duration: .3}
-  // },
+const SVG = styled.svg`
+  width: 300px;
+  height: 300px;
+  path {
+    stroke: white;
+    stroke-width: 2;
+  }
+`;
+
+// const boxVariant = {
+//   // hover: {scale: 1.5, rotateZ: 90},
+//   // tap: {borderRadius: '100px', scale: 1},
+//   // drag: {
+//   //   backgroundColor: 'rgb(46,204,113)',
+//   //   transition: {duration: .3}
+//   // },
   
+// }
+
+const svg = {
+  start: {
+    pathLength: 0,
+    fill: 'rgba(255,255,255,0)'
+  },
+  end: {
+    pathLength: 1,
+    fill: 'rgba(255,255,255,1)',
+    transition: {
+      default: {
+        duration: 3
+      },
+      fill: {
+        duration: 2,
+        delay: 2
+      }
+    }
+  }
 }
 
 function App() {
   // const biggerBoxRef = useRef<HTMLDivElement>(null);
   const x = useMotionValue(0);
-  const rotateZ = useTransform(x, [-500, 500], [-360, 360]);
+  // const rotateZ = useTransform(x, [-500, 500], [-360, 360]);
   const gradient = useTransform(x, [-500, 0, 500],[
     'linear-gradient(135deg,rgb(9, 170, 184),rgb(130, 208, 244))',
     'linear-gradient(135deg,rgb(238, 0, 153),rgb(221, 0, 238))',
     'linear-gradient(135deg,rgb(192, 22, 22),rgb(245, 113, 113))'
   ]);
-  const {scrollYProgress} = useViewportScroll();
-  const scale = useTransform(scrollYProgress, [0, 1], [1, 3]);
+  // const {scrollYProgress} = useViewportScroll();
+  // const scale = useTransform(scrollYProgress, [0, 1], [1, 3]);
   // useEffect(() => {
   //   x.onChange(() => console.log(x.get()));
   // },[x])
@@ -106,7 +135,21 @@ function App() {
       {/* <BiggerBox ref={biggerBoxRef}>
         <Box drag dragConstraints={biggerBoxRef} dragSnapToOrigin dragElastic={0.5} variants={boxVariant} whileHover='hover' whileTap='tap'/>
       </BiggerBox> */}
-      <Box style={{x, rotateZ, scale}}  drag='x' dragSnapToOrigin/>
+      {/* <Box style={{x, rotateZ, scale}}  drag='x' dragSnapToOrigin/> */}
+      <SVG
+        focusable="false"
+        xmlns='http://www.w3.org/2000/svg'
+        viewBox='0 0 448 512'  
+      >
+        <motion.path
+          variants={svg}
+          initial='start'
+          animate='end'
+          
+          d='M224 373.12c-25.24-31.67-40.08-59.43-45-83.18-22.55-88 112.61-88 90.06 0-5.45 24.25-20.29 52-45 83.18zm138.15 73.23c-42.06 18.31-83.67-10.88-119.3-50.47 103.9-130.07 46.11-200-18.85-200-54.92 0-85.16 46.51-73.28 100.5 6.93 29.19 25.23 62.39 54.43 99.5-32.53 36.05-60.55 52.69-85.15 54.92-50 7.43-89.11-41.06-71.3-91.09 15.1-39.16 111.72-231.18 115.87-241.56 15.75-30.07 25.56-57.4 59.38-57.4 32.34 0 43.4 25.94 60.37 59.87 36 70.62 89.35 177.48 114.84 239.09 13.17 33.07-1.37 71.29-37.01 86.64zm47-136.12C280.27 35.93 273.13 32 224 32c-45.52 0-64.87 31.67-84.66 72.79C33.18 317.1 22.89 347.19 22 349.81-3.22 419.14 48.74 480 111.63 480c21.71 0 60.61-6.06 112.37-62.4 58.68 63.78 101.26 62.4 112.37 62.4 62.89.05 114.85-60.86 89.61-130.19.02-3.89-16.82-38.9-16.82-39.58z'
+        ></motion.path>
+        
+      </SVG>
     </Wrapper>
   );
 }
